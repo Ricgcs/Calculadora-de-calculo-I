43637125a24eec34ab5593cb3e2c26fda670594e
ricgcs <rg530755@gmail.com>
2025-10-30 09:55:28 -0300

    divs 2.0

--- v3/back/index.js
+++ v3/back/index.js
@@ -8,9 +8,22 @@ app.post("/envio/string",(req,res)=>{
     let teste = req.body.dado
     res.json({res:'vambora'})
     console.log(teste)
-    
+    function tombo(obj)
+    {
+        if(obj == null){
+            console.log("INPUT vazio")
+        }
+        if(obj.potencia == 1) obj = obj.coeficiente
+        if(obj.varivel == null) obj = 0
+        else{
+            obj.coeficiente *= obj.potencia
+            obj.potenca -= 1
+        }
+    }
 })
 
 app.listen(3000,()=>{
     console.log("servidor funcionando")
 })
+
+
