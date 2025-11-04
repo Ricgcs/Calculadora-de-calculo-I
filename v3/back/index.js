b8475b82cead0dee6c858543b9577b6833a8a650
Felipe Batista <fbsantos019@gmail.com>
2025-11-03 23:35:54 -0300

    menu

--- v3/back/index.js
+++ v3/back/index.js
@@ -2,6 +2,48 @@ import express from 'express'
 import bodyParser from 'body-parser';
 import cors from "cors"
 let app = express();
+
+class Expressoes{
+    constructor(constante,variavel,potencia){
+        this.constante = constante
+        this.variavel = variavel
+        this.potencia = potencia
+    }
+}
+
+const prompt = require('prompt-sync')()
+console.log("!!!AVISO!!! - As expressões e operadores devem estar separados por espaço")
+console.log("Exemplo: 3x^2 + 2x^7 - 6 + 3/2x^4")
+const expresssao = prompt("Digite a expressão:")
+console.log("Escolha qual método você quer fazer:")
+console.log("1- Derivada")
+console.log("2 - Integral")
+console.log("3 - Substituir o valor de X")
+console.log("4 - Ponto Critico")
+console.log("5 - Bissecção")
+console.log("0 - Para sair da calculadora")
+let separacao = expresssao.split(" ")
+let operadores = ['+','-']
+const metodo = prompt()
+switch(Number(metodo)){
+    case 1:
+        tombo()
+    case 2:
+        integralTombo()
+    case 3:
+        substituirX()
+    case 4:
+        pontoCritico()
+    case 5:
+        console.log("Digite o inicio")
+        console.log("Digite o fim")
+        bisseccao()
+    default:
+        console.log("Saindo da Calculadora")
+        break
+}
+
+
 app.use(cors());
 app.use(express.json())
 app.post("/envio/string",(req,res)=>{
@@ -52,21 +94,43 @@ app.post("/envio/string",(req,res)=>{
         }
         return res
     }
+    function substituirX(obj,x){
+        obj.variavel = x
+        if(obj.coeficiente != null){
+            if(obj.potencia != null){
+                return obj.coeficiente*(obj.variavel**obj.potencia)
+            }
+            else{
+                return obj.coeficiente*obj.variavel
+            }
+        }
+        else{
+            if(obj.potencia != null){
+                return obj.variavel**obj.potencia
+            }
+            else{
+                return obj.variavel
+            }
+        }
+    }
+    function descobrirX(obj){
+        
+    }
     function pontoCritico(funcao){
-        if()
+
     }
     //Ainda não sei como colocar a função, se coloco como parametro ou dentro da função
     function bisseccao(inicio,fim,obj){
         var meio
-        if(valordeX(inicio) >= 0 || valordeX(fim) <= 0){
+        if(substituirX(obj,inicio) >= 0 || substituirX(obj,fim) <= 0){
             console.log("Intervalo Invalido")
             return 
         }
         while(inicio != fim){
             meio = inicio+fim/2
-            if(valordeX(meio) < 0)inicio = meio           
-            if(valordeX(meio) > 0) fim = meio
-            if(valordeX(meio) <= 0.0001){
+            if(substituirX(obj,meio) < 0)inicio = meio           
+            if(substituirX(obj,meio) > 0) fim = meio
+            if(substituirX(obj,meio) <= 0.0001){
                 return meio
             }
         }
