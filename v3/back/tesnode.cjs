2b1033c442cfb15076a63c8f1dfee7fe0e80063c
Felipe Batista <fbsantos019@gmail.com>
2025-11-05 00:31:42 -0300

    substuicao de x e tombo

--- v3/back/tesnode.cjs
+++ v3/back/tesnode.cjs
@@ -1,12 +1,9 @@
-import express from 'express'
-import bodyParser from 'body-parser';
-import cors from "cors"
-import { sep } from 'path';
-let app = express();
+const { sep } = require('path')
+const { serialize } = require('v8')
 
 class Expressoes{
-    constructor(constante,variavel,potencia){
-        this.constante = constante
+    constructor(coeficiente,variavel,potencia){
+        this.coeficiente = coeficiente
         this.variavel = variavel
         this.potencia = potencia
     }
@@ -26,41 +23,43 @@ console.log("0 - Para sair da calculadora")
 let separacao = expressao.split(" ")
 let operadores = ['+','-']
 for(let i = 0;i<separacao.length;i++){
-    if(separacao[i].includes(operadores)){
-        continue
+    if(separacao[i] == '+' || separacao[i] == '-'){
+        continue 
     }
-    if(separacao[i].includes("^")) divs = separacao[i].split(/(x^)/)
+    if(separacao[i].includes("^")) divs = separacao[i].split(/(x\^)/)
     else divs = separacao[i].split(/(x)/)
-    separacao[i] = new Expressoes(divs[0].divs[1],divs[2])
+    if(divs[2] == '') divs[2] = "1"
+    if(divs[0] == '') divs[0] = "1"
+    separacao[i] = new Expressoes(divs[0],divs[1],divs[2])
 }
-
-const metodo = prompt("")
+const metodo = prompt("método: ")
 switch(Number(metodo)){
     case 1:
-        tombo()
+        tombo(separacao)
+        break
     case 2:
-        integralTombo()
+        integralTombo(separacao)
+        break
     case 3:
-        substituirX()
+        const x = prompt("Digite valor de X: ")
+        substituirX(separacao,x)
+        break
     case 4:
         pontoCritico()
+        break
     case 5:
         console.log("Digite o inicio")
         console.log("Digite o fim")
         bisseccao()
+        break
     default:
         console.log("Saindo da Calculadora")
         break
 }
 
+console.log(separacao)
 
-app.use(cors());
-app.use(express.json())
-app.post("/envio/string",(req,res)=>{
-    let teste = req.body.dado
-    res.json({res:'vambora'})
-    console.log(teste)
-    function tombo(obj)
+function tombo(obj)
     {
         for(let i = 0;i<obj.length;i++){
             // if(obj[i].variavel == "sen(x)"){
@@ -78,14 +77,21 @@ app.post("/envio/string",(req,res)=>{
             // if(obj.variavel == "e^x"){
             //     continue
             // }
+            if(obj[i] == "+" || obj[i] == "-") continue
             if(obj[i] == null){
             console.log("INPUT vazio")
             }
             if(obj[i].potencia == 1) obj[i] = obj[i].coeficiente
-            if(obj[i].variavel == null) obj[i] = 0
+            else if(obj[i].variavel == null) obj[i] = 0
             else{
-                obj[i].coeficiente[0] *= obj[i].potencia
-                obj[i].potencia -= 1
+                if(obj[i].coeficiente.length > 1){
+                    obj[i].coeficiente[0] *= obj[i].potencia
+                    obj[i].potencia -= 1    
+                }
+                else{
+                    obj[i].coeficiente *= obj[i].potencia
+                    obj[i].potencia -= 1
+                }
             }
         }
     }
@@ -110,7 +116,6 @@ app.post("/envio/string",(req,res)=>{
                 console.log("INPUT vazio")
             }
             if(obj[i].variavel == null) obj[i].variavel = "x"
-            if(obj[i].potencia == -1) obj[i] = "Ln(x)"
             else{
                 obj[i].potencia += 1
                 //Falta a implementeção do tipo de coeficiente
@@ -121,72 +126,15 @@ app.post("/envio/string",(req,res)=>{
             }
         }
     }
-    function basckara(a,b,c){
-        var delta = b**2 - (4*a*c)
-        if(delta < 0){ 
-            console.log("Não Existe") 
-            return
-        }
-        var res
-        if(delta == 0){
-            res = -b/(2*a)
-        }
-        else{
-            var x1 = -b+delta/(2*a)
-            var x2 = -b-delta/(2*a)
-            res = [x1,x2]
-        }
-        return res
-    }
     function substituirX(obj,x){
         for(let i =0;i<obj.length;i++){
-            obj[i].variavel = x
-            if(obj[i].coeficiente != null){
-                if(obj[i].potencia != null){
-                    obj[i] = obj[i].coeficiente[0]*(obj[i].variavel**obj[i].potencia)
-                }
-                else{
-                    obj[i] = obj[i].coeficiente*obj[i].variavel
-                }
-            }
+            if(obj[i] == "+" || obj[i] == "-") continue
+            if(obj[i].variavel == null){
+                obj[i] = obj[i].coeficiente
+            } 
             else{
-                if(obj[i].potencia != null){
-                    obj[i] = obj[i].variavel**obj[i].potencia
-                }
-                else{
-                    obj[i] = obj[i].variavel
-                }
+                obj[i].variavel = x
+                obj[i] = obj[i].coeficiente[0]*(obj[i].variavel**obj[i].potencia)
             }
         }
     }
-    function descobrirX(obj){
-        
-    }
-    function pontoCritico(funcao){
-
-    }
-    //Ainda não sei como colocar a função, se coloco como parametro ou dentro da função
-    function bisseccao(inicio,fim,obj){
-        var meio
-        if(substituirX(obj,inicio) >= 0 || substituirX(obj,fim) <= 0){
-            console.log("Intervalo Invalido")
-            return 
-        }
-        while(inicio != fim){
-            meio = inicio+fim/2
-            if(substituirX(obj,meio) < 0)inicio = meio           
-            if(substituirX(obj,meio) > 0) fim = meio
-            if(substituirX(obj,meio) <= 0.0001){
-                return meio
-            }
-        }
-        console.log(`X não tem raiz no intervalo {${inicio},${fim}}`)
-    }
-    
-})
-
-app.listen(3000,()=>{
-    console.log("servidor funcionando")
-})
-
-
